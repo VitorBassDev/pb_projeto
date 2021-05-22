f2b24af56ac534a350739afdec1bb3722adb6010
Vitor Guedes <vittorbassdev@gmail.com>
2021-05-22 16:07:27 -0300

    project structure refactoring Styles CSS Bootstrap

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <>
+    
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+        <div className="alert alert-primary" role="alert">
+          Formulário 
+        </div>
+        </header>
+        <main>
+        <div class="row">
+
+        </div>
+        </main>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
